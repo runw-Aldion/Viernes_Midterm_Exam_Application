931af658f7ea301a220d148801e91ebfae3ec742
Aldion Viernes <aldion.i.viernes@gmail.com>
2026-02-20 00:00:56 +0000

    update: add product controller and web

---
+++ Viernes_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $theme = "Gadgets (Phones)";
+
+        $products = [
+            [
+                'brand' => 'Apple',
+                'model' => 'iPhone 15 Pro',
+                'storage' => '256GB',
+                'price' => 84990
+            ],
+            [
+                'brand' => 'Samsung',
+                'model' => 'Galaxy S24 Ultra',
+                'storage' => '256GB',
+                'price' => 78990
+            ],
+            [
+                'brand' => 'Huawei',
+                'model' => 'P60 Pro',
+                'storage' => '256GB',
+                'price' => 69990
+            ],
+            [
+                'brand' => 'Apple',
+                'model' => 'iPhone 13',
+                'storage' => '128GB',
+                'price' => 34990
+            ],
+            [
+                'brand' => 'Samsung',
+                'model' => 'Galaxy A55',
+                'storage' => '128GB',
+                'price' => 24990
+            ],
+        ];
+
+        return view('products.index', compact('theme', 'products'));
+    }
+}
